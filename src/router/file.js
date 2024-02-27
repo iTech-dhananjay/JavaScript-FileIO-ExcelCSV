d25daa6fef8ac525662bd79939a4fc1ae11194ed
Dhananjay <officialdhananjay76@gmail.com>
2024-02-27 17:29:12 +0530

    Fix path to read file

--- src/router/file.js
+++ src/router/file.js
@@ -8,11 +8,12 @@ import path from 'path'
 const router = express.Router();
 
 const filePath = '/Users/backend_development/Projects/JavaScript-FileIO-ExcelCSV/exports/files/temp.txt'
+//const filePath = '/Users/backend_development/Projects/JavaScript-FileIO-ExcelCSV/exports/files/Document.pdf'
 
 router.get('/readFile', async (req, res) => {
     try {
         const fileData = await fs.readFileSync(filePath);
-        
+
         res.status(200).json({success: true, fileData});
     } catch (error) {
         console.error('Error reading file:', error);
